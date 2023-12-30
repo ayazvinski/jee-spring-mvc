39fef951db520bcf278231901924137575d28955
ayazvinski <a.yazvinski@limitless.pl>
2023-12-30 11:54:19 +0100

    Widoki zadania 1,2

--- src/main/java/pl/coderslab/controller/HelloController.java
+++ src/main/java/pl/coderslab/controller/HelloController.java
@@ -1,10 +1,15 @@
 package pl.coderslab.controller;
 
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.time.LocalTime;
 
 @Controller
 public class HelloController {
@@ -16,5 +21,19 @@ public class HelloController {
     public String helloWorld(){
         logger.debug("My first log");
         return "Hello world!";
+    }
+
+    @GetMapping("/helloView")
+    public String helloView(Model model){
+        LocalTime localtime = LocalTime.of(1,0);
+        if(localtime.isAfter(LocalTime.of(8, 0)) &&
+                localtime.isBefore(LocalTime.of(20, 0))){
+            model.addAttribute("color", "black");
+            model.addAttribute("backgroundColor", "white");
+        }else{
+            model.addAttribute("color", "white");
+            model.addAttribute("backgroundColor", "black");
+        }
+        return "home";
     }
 }
